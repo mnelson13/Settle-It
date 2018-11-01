e22922ab92741a97d465212fab0240c749c0864a
meh <spencerdberg1@gmail.com>
2018-11-01 12:56:02 -0500

    added some models

---
+++ models/Users.js
@@ -0,0 +1,13 @@
+module.exports = function(sequelize, DataTypes){
+    var Users = sequelize.define("Users",{
+        Email:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Password:{
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+    return Users;
+}
