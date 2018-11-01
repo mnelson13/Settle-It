55df57fdd06e721cb2a8f8fcbebe9f0c3e21bc88
Chase Holladay <chaseholladay@gmail.com>
2018-11-01 14:28:37 -0500

    working on local.js

---
+++ passport/authorization/local.js
@@ -0,0 +1,15 @@
+
+//configure local authentication strategy with user adding their username and password. this strategy requires a callback "verify" which accepts their credentials and calls "done" providing a user.
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (!user.verifyPassword(password)) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
+
+  
