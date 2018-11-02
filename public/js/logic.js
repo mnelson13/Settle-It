bef09d17c8657a351afe352acf263c3d2d4705b2
MNelson <melissa.nelson13@yahoo.com>
2018-11-02 13:02:31 -0500

    added some jquery

--- public/js/logic.js
+++ public/js/logic.js
@@ -3,4 +3,76 @@ $(document).ready(function(){
     $('.modal').modal();
     // Materialize jquery for Collapsable
     $('.collapsible').collapsible();
-  });
+
+
+
+
+
+  //Main Page
+
+  //voting
+  let votes;
+
+  $(document.body).on("click", ".voteA", function(){
+    let id = $(this).data("settleId");
+
+    $.get("/api/getOne/" + id, function(data){
+      votes = data.Side_A_Points + 1
+
+    }).then(function(){
+      let newVotes = {
+        votes: votes
+      }
+      $.ajax("api/settles/votes/sideA/" + id,{
+        type: "PUT",
+        data: newVotes
+      }).then(function(){
+        console.log("udated id: " + id);
+        location.reload();
+        //code to update votes on DOM, but not reload whole page??
+      })
+    })
+  })
+
+
+
+
+
+
+
+
+  //Account Page
+
+
+
+
+
+
+
+
+
+
+
+  //Create Page  
+
+$("#newSettle").on("submit",function(event){
+  event.preventDefault();
+
+  let newSettle = {
+  
+  }
+
+
+  $.post("/api/settles", newSettle)
+    .then(function(){
+      console.log("added a new settle");
+      location.reload();
+    })
+
+})
+
+
+  
+
+
+});
