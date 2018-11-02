6fa52ec7271f01f225ea4a46c8c0dca767b75543
Chase Holladay <chaseholladay@gmail.com>
2018-11-01 22:22:00 -0500

    added passport configuration to server.js. added functions to local.js

--- passport/authorization/local.js
+++ passport/authorization/local.js
@@ -1,15 +1,37 @@
 
 //configure local authentication strategy with user adding their username and password. this strategy requires a callback "verify" which accepts their credentials and calls "done" providing a user.
+//documentation used from passport.js.org//
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 
+  //more configuration and verify callback that helps to find the user that has a set of credentials, username and password//
 passport.use(new LocalStrategy(
-    function(username, password, done) {
-      User.findOne({ username: username }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (!user.verifyPassword(password)) { return done(null, false); }
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); } //occurs if database not available//
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
         return done(null, user);
-      });
-    }
-  ));
+    });
+  }
+));
 
-  
+//possible form that we can style later. Use for user to enter their credentials and log in//
+//<form action="/login" method="post">
+//  <div>
+//      <label>Username:</label>
+//      <input type="text" name="username"/>
+//  </div>
+//  <div>
+//      <label>Password:</label>
+//      <input type="password" name="password"/>
+//  </div>
+//  <div>
+//      <input type="submit" value="Log In"/>
+//  </div>
+//</form>
+
---
+++ passport/routes/local-authentication-routes.js
@@ -0,0 +1,21 @@
+var 
+
+const 
+
+
+
+
+
+
+
+
+
+
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
+//add a redirect to the function above
